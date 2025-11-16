061508eb1fcdebbbf14a302ed95d4aa9d1b84b59
Felipe Alves <felipealves1818@gmail.com>
2025-11-16 18:46:43 -0300

    ajsutando variaveis direto no docker-compose e adc cp do env e comando schedule:work no bash de start

--- fiz-o-meu-melhor/start.sh
+++ fiz-o-meu-melhor/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Subindo containers..."
+cp .env.example .env
+
 docker compose up -d --build
 
 echo "Ajustando permissões da pasta storage..."
@@ -11,3 +13,4 @@ docker exec -u 0 -it app-laravel composer install
 docker exec -it app-laravel php artisan migrate --seed
 docker exec -i postgres-db psql -U root -d postgres -c "SELECT 1 FROM pg_database WHERE datname = 'app_test'" | grep -q 1 || docker exec -i postgres-db psql -U root -d postgres -c "CREATE DATABASE app_test;"
 echo "Banco de testes app_test criado/verificado"
+docker exec -it app-laravel php artisan schedule:work
